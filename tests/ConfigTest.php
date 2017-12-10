fc52a9bb1321d083b56eebeed655894d9fef12ed
Tim Joosten <Tjoosten3@gmail.com>
2017-12-10 08:26:51 +0100

    Implement validation library

---
+++ tests/ConfigTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Artisan;
+
+/**
+ * Class ConfigTest
+ */
+class ConfigTest extends TestCase
+{
+    /**
+     * @test 
+     */
+    public function config_file_is_published()
+    {
+        $configFile = __DIR__.'/../vendor/laravel/laravel/config/form-helpers.php';
+        File::delete($configFile);
+
+        $this->assertFileNotExists($configFile);
+        Artisan::call('vendor:publish', [
+            '--provider' => 'ActivismeBE\FormHelper\FormServiceProvider',
+        ]);
+
+        $this->assertFileExists($configFile);
+    }
+}
---
+++ tests/ValueTest.php
@@ -0,0 +1,44 @@
+<?php 
+
+use ActivismeBE\FormHelper\Form; 
+use Illuminate\Database\Eloquent\Model; 
+
+/**
+ * Class ValueTest
+ */
+class ValueTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function value_method_returns_session_empty_values() 
+    {
+        $form = app('Activisme_BE'); 
+        session()->flashInput(['field' => '']); 
+
+        $this->assertEquals('', $form->value('field'));
+    }
+
+    /**
+     * @test
+     */
+    public function value_method_returns_model_empty_values() 
+    {
+        $model = $this->prophesize(Model::class); 
+        $model->getAttribute('field')->willReturn('');
+
+        $form = app('Activisme_BE'); 
+        $form->model($model->reveal()); 
+
+        $this->assertEquals('', $form->value('field'));
+    }
+
+    /**
+     * @test
+     */
+    public function value_method_returns_default_value() 
+    {
+        $form = app('Activisme_BE'); 
+        $this->assertEquals('default', $form->value('field', 'default'));
+    }
+}
