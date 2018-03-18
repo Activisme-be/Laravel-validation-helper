29b4513e637c6ea7fb7cdb0939ab1c2a37c02973
Tim Joosten <Tjoosten@users.noreply.github.com>
2018-03-19 00:20:26 +0100

    V 1.1.0 (#11)
    * Fix #6 | Drop php 7.0 dependency
    
    * Implementatie Testdox attributes (#5)
    
    * Fix #10 | Implement changelog (#14)
    
    * Implement codecov (#15)
    
    * Fix #12 | Typehint service provider (#16)
    
    * Implement phpunit config (#17)
    
    * Fix #17 | Typo in package name
    
    * Fix #19 | Prepare package for laravel 5.6
    
    * Fix #13 | Implementatie scripts composer

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `activismebe/laravel-form-helpers` will be documented in this file
+
+## 1.0.0 - 2017-10-12
+
+- Initial release
--- tests/EscapeValueTest.php
+++ tests/EscapeValueTest.php
@@ -6,41 +6,46 @@
 class EscapeValueTest extends TestCase
 {
     /**
-     *  @test
+     * @test
+     * @testdox Excape value for an input field
      */
-    public function escape_value_for_an_input_field()
+    public function escape_value_for_an_input_field(): void
     {
         $this->assertBladeRender('name="name" value="&lt;"', "@input('name', '<')");
     }
 
     /**
      * @test
+     * @testdox Excape value for a textarea
      */
-    public function escape_value_for_a_textarea()
+    public function escape_value_for_a_textarea(): void
     {
         $this->assertBladeRender('&lt;html&gt;', "@text('name', '<html>')");
     }
 
     /**
      * @test
+     * @testdox Excape value for a checkbox
      */
-    public function escape_value_for_a_checkbox()
+    public function escape_value_for_a_checkbox(): void
     {
         $this->assertBladeRender('name="name" value="&lt;"', "@checkbox('name', '<')");
     }
 
     /**
      * @test
+     * @testdox Escape value for a radio button
      */
-    public function escape_value_for_a_radio_button()
+    public function escape_value_for_a_radio_button(): void
     {
         $this->assertBladeRender('name="name" value="&lt;"', "@radio('name', '<')");
     }
 
     /**
      * @test
+     * @testdox Escape valie for an option
      */
-    public function escape_value_for_an_option()
+    public function escape_value_for_an_option(): void
     {
         $this->assertBladeRender('<option value="&lt;">Text</option>', '@options($options, "name")', [
             'options' => ['<' => 'Text']
